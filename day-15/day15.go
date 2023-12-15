1e13dcf0d330530e21a163fd8091b078810e1a0b
Dmytro Donukis <dmytro.donukis@gmail.com>
2023-12-15 22:30:50 +0100

    day 15: part 2 done

--- day-15/day15.go
+++ day-15/day15.go
@@ -7,7 +7,61 @@ import (
 	"log"
 	"os"
 	"slices"
+	"strconv"
+	"strings"
 )
+
+type Box struct {
+	id     int
+	keys   map[string]int
+	values []int
+}
+
+func (b *Box) Add(label string, focalLength int) {
+	index, exists := b.keys[label]
+	if exists {
+		b.values[index] = focalLength
+	} else {
+		index = len(b.values)
+		b.values = append(b.values, focalLength)
+		b.keys[label] = index
+	}
+}
+
+func (b *Box) Remove(label string) {
+	index, exists := b.keys[label]
+	if exists {
+		b.values[index] = -1
+		delete(b.keys, label)
+	}
+}
+
+func (b *Box) String() string {
+	strParts := make([]string, 1, len(b.keys)+1)
+	strParts[0] = fmt.Sprintf("Box %d:", b.id)
+	for k, idx := range b.keys {
+		p := fmt.Sprintf("[%s %d]", k, b.values[idx])
+		strParts = append(strParts, p)
+	}
+	return strings.Join(strParts, " ")
+}
+
+func (b *Box) FocusingPower() int {
+	fp := 0
+	boxNo := b.id + 1
+	slotNo := 0
+	for _, focalLength := range b.values {
+		if focalLength > 0 {
+			slotNo++
+			fp += boxNo * slotNo * focalLength
+		}
+	}
+	return fp
+}
+
+func NewBox(id int) *Box {
+	return &Box{id: id, keys: make(map[string]int), values: make([]int, 0, 1)}
+}
 
 func main() {
 	filePath := os.Args[1]
@@ -18,15 +72,49 @@ func main() {
 
 	sequenceScanner := bufio.NewScanner(f)
 	sequenceScanner.Split(splitSequence)
+
 	total := 0
+	storage := make([]*Box, 256)
+	totalFocusingPower := 0
 	for sequenceScanner.Scan() {
-		token := sequenceScanner.Text()
-		tokenHash := hash(token)
-		fmt.Printf("'%s' -> %d\n", sequenceScanner.Text(), tokenHash)
-		total += tokenHash
+		instruction := sequenceScanner.Text()
+		instructionHash := hash(instruction)
+		total += instructionHash
+
+		execute(instruction, storage)
+	}
+	for _, b := range storage {
+		if b != nil {
+			fmt.Println(b)
+			totalFocusingPower += b.FocusingPower()
+		}
 	}
 	fmt.Printf("Hash sum (P1): %d\n", total)
+	fmt.Printf("Focusing power (P2): %d\n", totalFocusingPower)
 
+}
+
+func execute(instruction string, storage []*Box) {
+	if idx := strings.Index(instruction, "-"); idx > 0 {
+		label := instruction[:idx]
+		boxId := hash(label)
+		if b := storage[boxId]; b != nil {
+			b.Remove(label)
+		}
+	} else {
+		labelAndFocalLength := strings.Split(instruction, "=")
+		boxId := hash(labelAndFocalLength[0])
+		focalLength, err := strconv.Atoi(labelAndFocalLength[1])
+		if err != nil {
+			log.Fatalf("Cannot parse instruction '%s': %v\n", instruction, err)
+		}
+		b := storage[boxId]
+		if b == nil {
+			b = NewBox(boxId)
+			storage[boxId] = b
+		}
+		b.Add(labelAndFocalLength[0], focalLength)
+	}
 }
 
 func hash(s string) (value int) {
