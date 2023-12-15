e7cafcb6f160dfe927bdc2fbb4cfed8fe5b56315
Dmytro Donukis <dmytro.donukis@cognite.com>
2023-12-15 13:21:32 +0100

    day 14 part 1 done

--- day-14/day14.go
+++ day-14/day14.go
@@ -7,6 +7,15 @@ import (
 	"os"
 )
 
+type Rock struct {
+	movable  bool
+	position int
+}
+
+type Column []Rock
+
+type Board []Column
+
 func main() {
 	filepath := os.Args[1]
 	f, err := os.Open(filepath)
@@ -21,24 +30,18 @@ func main() {
 	}
 	line := fScanner.Text()
 	rowIndex := 0
-	unmovableCols := make([][]int, len(line))
-	movableCols := make([][]int, len(line))
-	parseLine(line, rowIndex, unmovableCols, movableCols)
+	board := make(Board, len(line))
+	parseLine(line, rowIndex, board)
+
 	for fScanner.Scan() {
 		rowIndex++
 		line = fScanner.Text()
-		parseLine(line, rowIndex, unmovableCols, movableCols)
-		// fmt.Println(line)
+		parseLine(line, rowIndex, board)
 	}
 
 	totalP1 := 0
-
-	// i := 0
-	// colLoad := calcColLoad(rowIndex+1, unmovableCols[i], movableCols[i])
-	// fmt.Printf("\nCol %d: %d\n\n", i, colLoad)
-
 	for i := 0; i < len(line); i++ {
-		colLoad := calcColLoad(rowIndex+1, unmovableCols[i], movableCols[i])
+		colLoad := calcColLoad(rowIndex+1, board[i])
 		fmt.Printf("\nCol %d: %d\n\n", i, colLoad)
 		totalP1 += colLoad
 	}
@@ -46,13 +49,13 @@ func main() {
 
 }
 
-func parseLine(line string, row int, unmovableCols, movableCols [][]int) {
+func parseLine(line string, row int, board Board) {
 	for i, r := range line {
 		switch r {
 		case 'O':
-			movableCols[i] = append(movableCols[i], row)
+			board[i] = append(board[i], Rock{true, row})
 		case '#':
-			unmovableCols[i] = append(unmovableCols[i], row)
+			board[i] = append(board[i], Rock{false, row})
 		default:
 			continue
 		}
@@ -71,47 +74,55 @@ func calcCumLoad(rowIndex, count, totalRows int) int {
 	return total
 }
 
+func calcColLoad(totalRows int, col Column) int {
+	totalLoad := 0
+	rangeStart := 0
+	movableInRange := 0
+	for _, rock := range col {
+		if rock.movable {
+			movableInRange++
+		} else {
+			totalLoad += calcCumLoad(rangeStart, movableInRange, totalRows)
+			rangeStart = rock.position + 1
+			movableInRange = 0
+		}
+	}
+	if movableInRange > 0 {
+		totalLoad += calcCumLoad(rangeStart, movableInRange, totalRows)
+	}
+	return totalLoad
+}
+
 func calcColLoad(totalRows int, unmovablePositions, movablePositions []int) int {
 	var totalLoad, fromRow, toRow, lastMovableIdx int
 	var totalRocks int
 
 	for i := 0; i < len(unmovablePositions)+1; i++ {
 		// find how many movable rocks between current unmovable and next unmovable (or end of grid)
-		// fmt.Println()
 		if i < len(unmovablePositions) {
 			toRow = unmovablePositions[i]
 		} else {
 			toRow = totalRows
 		}
-		// fmt.Printf("%d -> %d\n", fromRow, toRow)
 		if fromRow == toRow {
 			continue
 		}
-		// fmt.Printf("lastMovableIdx: %d\n", lastMovableIdx)
 		movablePositions = movablePositions[lastMovableIdx:]
-		// fmt.Printf("movablePositions[%d:]\n", lastMovableIdx)
-		// fmt.Println(movablePositions)
 		totalMovableRocksInRange := 0
 		for j := 0; j < len(movablePositions); j++ {
-			// fmt.Printf("j: %d\n", j)
 			if movablePositions[j] < toRow {
 				totalMovableRocksInRange++
 			} else {
 				lastMovableIdx = j
-				// fmt.Printf("lastMovableIdx = %d\n", lastMovableIdx)
 				break
 			}
 		}
-
-		// fmt.Printf("totalMovableRocksInRange: %d\n", totalMovableRocksInRange)
 		totalLoad += calcCumLoad(fromRow, totalMovableRocksInRange, totalRows)
 		fromRow = toRow + 1
 		totalRocks += totalMovableRocksInRange
-		// fmt.Printf("load: %d\n", totalLoad)
 		if totalMovableRocksInRange == len(movablePositions) {
 			break
 		}
 	}
-	// fmt.Printf("movable rocks: %d\n", totalRocks)
 	return totalLoad
 }
